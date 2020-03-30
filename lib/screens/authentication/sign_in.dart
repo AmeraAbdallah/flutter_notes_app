14fe612c914caced37b56808c55c92f173ed7c38
Amera Ayad <amera.ayad.it@gmail.com>
2020-03-30 22:49:51 +0200

    add firebase login authentication functionality with form validation

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -15,6 +15,9 @@ class _SignInState extends State<SignIn> {
 
   String email = '';
   String password = '';
+  bool _emailValidate = false;
+  bool _passwordValidate = false;
+  String error = '';
   
   final AuthenticationService _auth = AuthenticationService();
 
@@ -40,10 +43,11 @@ class _SignInState extends State<SignIn> {
                       fontSize: 18,
                     ),
                     hintText: "Type in your Email",
-                    fillColor: Colors.deepPurple[50]
+                    fillColor: Colors.deepPurple[50],
+                    errorText:  _emailValidate ? 'Email Can\'t Be Empty.' : null,
                   ),
                   onChanged: (val) {
-
+                    setState(() => email = val);
                   },
               ),
               SizedBox(height: 20,),
@@ -61,11 +65,12 @@ class _SignInState extends State<SignIn> {
                     ),
                     hintText: "Type in your Password",
                     fillColor: Colors.deepPurple[50],
+                    errorText: _passwordValidate ? 'Password length Can\'t Be less than 6.' : null,
                 ),
                 obscureText: true,
                 onChanged: (val) {
-
-                },
+                    setState(() => password = val);
+                  },
               ),
               SizedBox(height: 20,),
               ButtonTheme(
@@ -85,7 +90,22 @@ class _SignInState extends State<SignIn> {
                       borderRadius: new BorderRadius.circular(50.0),
                       // side: BorderSide(color: Colors.red),
                     ),
-                    onPressed: () {}
+                    onPressed: () async {
+                        if(email.length == 0) {
+                          setState(() => _emailValidate = true);
+                        }
+                        if(password.length < 6) {
+                          setState(() => _passwordValidate = true);
+                        }
+                        if(email.length != 0 && password.length >= 6) {
+                          setState(() => _passwordValidate = false);
+                          setState(() => _emailValidate = false);
+                          dynamic result = await _auth.signInUserUsingEmailAndPassword(email, password);
+                          if(result == null) {
+                            setState(() => error = "Wrong Email or password!");
+                          }
+                        }
+                    }
                   )
                 ),
                 Row(
@@ -103,6 +123,11 @@ class _SignInState extends State<SignIn> {
                     )
                   ],
                   mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red),
                 )
             ],
           )
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -34,6 +34,17 @@ class AuthenticationService {
     }
   }
 
+    Future signInUserUsingEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser firebaseUser = result.user;
+      return _fromFirebaseUserToUser(firebaseUser);
+    } catch(err) {
+      print(err.toString());
+      return null;
+    }
+  }
+
   Future logout() async {
     try {
       return await _auth.signOut();
