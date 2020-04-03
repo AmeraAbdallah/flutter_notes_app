a6e502b8e26d70dd2e06cd154420420ca5caac55
Amera Ayad <amera.ayad.it@gmail.com>
2020-04-03 20:48:08 +0200

    fix ui for signup screen , later will make it readable

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:note_app/services/auth.dart';
 import 'package:loading/loading.dart';
 
-class Register extends StatefulWidget {
+extension ColorExtension on String {
+  toColor() {
+    var hexColor = this.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+  }
+}
 
+class Register extends StatefulWidget {
   final Function toggleIsSignInView;
   Register({this.toggleIsSignInView});
 
@@ -12,8 +23,6 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-
-
   String email = '';
   String password = '';
   bool _emailValidate = false;
@@ -21,131 +30,173 @@ class _RegisterState extends State<Register> {
   String error = '';
   bool _isLoading = false;
   final AuthenticationService _auth = AuthenticationService();
-  
+
   @override
   Widget build(BuildContext context) {
-    return _isLoading? Loading():
-    Scaffold(
-        backgroundColor: Colors.blue[900],
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 50,),
-              new TextField(
-                decoration: new InputDecoration(
-                    border: new OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(50.0),
+    return _isLoading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.white,
+            body: Container(
+                child: Column(
+              children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Container(
+                  // padding: Padding.,
+                  color: "#16DB93".toColor(),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      "Note",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40
                       ),
-                    ),
-                    filled: true,
-                    hintStyle: new TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 18,
-                    ),
-                    hintText: "Type in your Email",
-                    fillColor: Colors.deepPurple[50],
-                     errorText: _emailValidate ? 'Email Can\'t Be Empty' : null,
+                      ),
                   ),
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-              ),
-              SizedBox(height: 20,),
-              new TextField(
-                decoration: new InputDecoration(
-                    border: new OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(50.0),
-                      ),
-                    ),
-                    filled: true,
-                    hintStyle: new TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 18,
-                    ),
-                    hintText: "Type in your Password",
-                    fillColor: Colors.deepPurple[50],
-                    errorText: _passwordValidate ? 'Password length Can\'t Be less than 6.' : null,
+                  // child: Text("Note"),
                 ),
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() {
-                    password= val;
-                  });
-                },
               ),
-              SizedBox(height: 20,),
-              ButtonTheme(
-                minWidth: 180,
-                height: 50,
-                child: RaisedButton(
-                  child: Text(
-                    "Register", 
-                    style: 
-                    TextStyle(
-                      color: Colors.white70,
-                      fontSize: 20,
-                      ),
-                    ), 
-                    color: Colors.amber[900], 
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      // side: BorderSide(color: Colors.red),
-                    ),
-                    onPressed: () async {
-                        if(email.length == 0) {
-                          setState(() => _emailValidate = true);
-                        }
-                        if(password.length < 6) {
-                          setState(() => _passwordValidate = true);
-                        }
-                        if(email.length != 0 && password.length >= 6) {
-                          setState((){
-                            _passwordValidate = false;
-                            _emailValidate = false;
-                            _isLoading = true;
-                          });
+                new Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    // child: Align(
+                    //   alignment: Alignment.bottomCenter,
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            decoration: new InputDecoration(
+                              border: new OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: "#707070".toColor(), width: 2),
+                              ),
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: "#A4036F".toColor()),
+                              filled: true,
+                              hintStyle: new TextStyle(
+                                color: '#707070'.toColor(),
+                                fontSize: 18,
+                              ),
+                              hintText: "Johndoe@gmail.com",
+                              fillColor: Colors.white,
+                              errorText: _emailValidate
+                                  ? 'Email Can\'t Be Empty'
+                                  : null,
+                              focusedBorder:OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.pink, width: 2.0),
+                              ),
+                            ),
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          new TextField(
+                            decoration: new InputDecoration(
+                              border: new OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: "#707070".toColor(), width: 2),
+                              ),
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: "#A4036F".toColor()),
+                              filled: true,
+                              hintStyle: new TextStyle(
+                                color: '#707070'.toColor(),
+                                fontSize: 18,
+                              ),
+                              hintText: "*******",
+                              fillColor: Colors.white,
+                              errorText: _passwordValidate
+                                  ? 'Password length Can\'t Be less than 6.'
+                                  : null,
+                              focusedBorder:OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.pink, width: 2.0),
+                              ),
+                            ),
+                            obscureText: true,
+                            onChanged: (val) {
+                              setState(() {
+                                password = val;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          ButtonTheme(
+                              minWidth: double.infinity,
+                              height: 50,
+                              child: RaisedButton(
+                                  child: Text(
+                                    "Register",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  color: "#8C86AA".toColor(),
+                                  onPressed: () async {
+                                    if (email.length == 0) {
+                                      setState(() => _emailValidate = true);
+                                    }
+                                    if (password.length < 6) {
+                                      setState(() => _passwordValidate = true);
+                                    }
+                                    if (email.length != 0 &&
+                                        password.length >= 6) {
+                                      setState(() {
+                                        _passwordValidate = false;
+                                        _emailValidate = false;
+                                        _isLoading = true;
+                                      });
 
-                          dynamic result = await _auth.createUserUsingEmailAndPassword(email, password);
-                          if(result == null) {
-                            setState(() {
-                              error = "Not valid Email";
-                              _isLoading = false;
-                            });
-                          }
-                        }
-                    }
-                  )
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      'Already have an account?',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    FlatButton(
-                      textColor: Colors.blue,
-                      child: Text(
-                        'Sign in',
-                        style: TextStyle(fontSize: 20),
+                                      dynamic result = await _auth
+                                          .createUserUsingEmailAndPassword(
+                                              email, password);
+                                      if (result == null) {
+                                        setState(() {
+                                          error = "Not valid Email";
+                                          _isLoading = false;
+                                        });
+                                      }
+                                    }
+                                  })),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                'Already have an account?',
+                                style: TextStyle(
+                                    color: "#707070".toColor(), fontSize: 18),
+                              ),
+                              FlatButton(
+                                textColor: "#707070".toColor(),
+                                child: Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      decoration: TextDecoration.underline),
+                                ),
+                                onPressed: () {
+                                  widget.toggleIsSignInView();
+                                },
+                              )
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            error,
+                            style: TextStyle(color: Colors.red),
+                          )
+                        ],
                       ),
-                      onPressed: () {
-                        widget.toggleIsSignInView();
-                      },
-                    )
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                SizedBox(height: 20,),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red),
-                )
-            ],
-          )
-        )
-      );
+                    // )
+                  ),
+              ],
+            )));
   }
 }
