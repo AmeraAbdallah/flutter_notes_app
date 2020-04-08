249fa075182d31290bf9ae8c7c3f6c0779a09894
Amera Ayad <amera.ayad.it@gmail.com>
2020-04-08 14:09:53 +0200

    finish note details screen

--- lib/screens/NoteDetails.dart
+++ lib/screens/NoteDetails.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/note.dart';
+import 'package:note_app/services/auth.dart';
+import 'package:note_app/style.dart';
 
 class NoteDetails extends StatelessWidget {
+  Note note;
+  NoteDetails({this.note});
+  final AuthenticationService _auth = AuthenticationService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("note details"),
+        backgroundColor: Style.grey,
+        actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  "Logout",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                onPressed: () async {
+                  await _auth.logout();
+                },
+              )
+        ],
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      body: Column(children: <Widget>[
+        Container(
+            width: double.infinity,
+            height: 175,
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
+            color: Style.yellow,
+            child: Text(
+            note.title,
+            style: TextStyle(
+              color: Style.textColor,
+              fontSize: 24
+            ),
+          ),
         ),
-      ),
+        Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
+            child: Text(
+              note.description,
+              style: TextStyle(
+                color: Style.textColor,
+                fontSize: 20
+              ),
+            ),
+          )
+      ],)
     );
   }
 }
--- lib/screens/home/notesList.dart
+++ lib/screens/home/notesList.dart
@@ -55,7 +55,7 @@ class _NotesListState extends State<NotesList> {
               onTap: (){
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => NoteDetails()),
+                  MaterialPageRoute(builder: (context) => NoteDetails(note: entries[index])),
                 );
               },
               child: Container(
